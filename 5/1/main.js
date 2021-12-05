51d2e70b28a1154f4b715bb2d1df69aad0a4c0b7
Alex Untertrifaller <alex.untertrifaller@infominds.eu>
2021-12-05 20:01:41 +0100

    add 5

---
+++ 5/1/main.js
@@ -0,0 +1,58 @@
+var array = require('fs').readFileSync('../input.txt').toString().split("\n").map(String);
+
+var lines = {};
+
+function addPoint(x, y) {
+    const startPoint = x + ',' + y;
+    if (!lines.hasOwnProperty(startPoint))
+        lines[startPoint] = 1;
+    else
+        lines[startPoint] = lines[startPoint] + 1;
+}
+
+function drawLine(start, end) {
+    const x1 = Number(start[0]);
+    const y1 = Number(start[1]);
+
+    const x2 = Number(end[0]);
+    const y2 = Number(end[1]);
+
+    if (x1 === x2 || y1 === y2) {
+        if (x1 < x2) {
+            for (var i = x1; i <= x2; i++) {
+                addPoint(i, y1)
+            }
+        } else if (y1 < y2) {
+            for (var i = y1; i <= y2; i++) {
+                addPoint(x1, i)
+            }
+        } else if (x1 > x2) {
+            for (var i = x2; i <= x1; i++) {
+                addPoint(i, y1)
+            }
+        } else if (y1 > y2) {
+            for (var i = y2; i <= y1; i++) {
+                addPoint(x1, i)
+            }
+        }
+    }
+}
+
+for (var i = 0; i < array.length; i++) {
+    var tmp = array[i].split(' -> ');
+    var startPoint = tmp[0].split(',')
+    var endPoint = tmp[1].split(',')
+
+    // console.log(startPoint, endPoint)
+    drawLine(startPoint, endPoint)
+}
+
+// console.log(lines);
+
+var count = 0;
+for (var key in lines) {
+    if (lines[key] > 1)
+        count++;
+}
+
+console.log(count);
